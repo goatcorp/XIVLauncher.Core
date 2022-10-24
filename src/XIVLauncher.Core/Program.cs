ad6b7019e5e402f141be908092a66df118a31e44
goat <goatsdev@protonmail.com>
2022-10-24 02:04:05 +0200

    chore: only do update warnings on flatpaks, no pressure on other maintainers

--- src/XIVLauncher.Core/Program.cs
+++ src/XIVLauncher.Core/Program.cs
@@ -202,13 +202,15 @@ class Program
 
         var needUpdate = false;
 
+#if FLATPAK
         if (Config.DoVersionCheck ?? false)
         {
             var versionCheckResult = UpdateCheck.CheckForUpdate().GetAwaiter().GetResult();
 
             if (versionCheckResult.Success)
                 needUpdate = versionCheckResult.NeedUpdate;
-        }
+        }   
+#endif
 
         launcherApp = new LauncherApp(storage, needUpdate);
 
