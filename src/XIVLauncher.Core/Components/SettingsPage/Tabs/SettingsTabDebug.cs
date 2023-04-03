2344389f3d54ab95e46a5ff49c5db2cd6e038ae2
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2023-04-03 13:16:02 +0100

    Minor app UI cleanup

--- src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabDebug.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/Tabs/SettingsTabDebug.cs
@@ -6,26 +6,45 @@ namespace XIVLauncher.Core.Components.SettingsPage.Tabs;
 public class SettingsTabDebug : SettingsTab
 {
     public override SettingsEntry[] Entries => Array.Empty<SettingsEntry>();
-    public override string Title => "Debug Info";
+    public override string Title => "Debug";
 
     public override void Draw()
     {
+        ImGui.TextUnformatted("Generic Information");
+        ImGui.Separator();
+        ImGui.TextUnformatted($"Operating System: {Environment.OSVersion}");
+        ImGui.TextUnformatted($"Runtime Version: {Environment.Version}"); 
+        
         if (Program.IsSteamDeckHardware)
-            ImGui.Text("Is Steam Deck hardware");
+            ImGui.Text("Steam Deck Hardware Detected");
 
         if (Program.IsSteamDeckGamingMode)
-            ImGui.Text("Is Steam Deck");
+            ImGui.Text("Steam Deck Gaming Mode Detected");
 
+#if FLATPAK
+            ImGui.Text("Running as a Flatpak");
+#endif
+        
+        ImGui.Spacing();
+
+        ImGui.TextUnformatted("Environment Information");
         ImGui.Separator();
-
-        ImGui.PushTextWrapPos(0);
-
-        foreach (DictionaryEntry entry in Environment.GetEnvironmentVariables())
-        {
-            ImGui.TextUnformatted($"{entry.Key}={entry.Value}");
-        }
-
-        ImGui.PopTextWrapPos();
+            if(ImGui.BeginTable("EnvironmentTable", 2, ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg | ImGuiTableFlags.Resizable | ImGuiTableFlags.ScrollY))
+            {
+                ImGui.TableSetupScrollFreeze(0, 1);
+                ImGui.TableSetupColumn("Key", ImGuiTableColumnFlags.WidthStretch, 0.35f);
+                ImGui.TableSetupColumn("Value");
+                ImGui.TableHeadersRow();
+                foreach (DictionaryEntry entry in Environment.GetEnvironmentVariables())
+                {   
+                    ImGui.TableNextRow();
+                    ImGui.TableNextColumn();
+                    ImGuiHelpers.TextWrapped(entry.Key?.ToString() ?? "null");
+                    ImGui.TableNextColumn();
+                    ImGuiHelpers.TextWrapped(entry.Value?.ToString() ?? "null");
+                }
+                ImGui.EndTable();
+            }
 
         base.Draw();
     }
