bd5d622e30183b189df0f5793a0680056e1a832e
Blooym <19539165+Blooym@users.noreply.github.com>
2024-11-28 13:28:06 +0000

    refactor: remove flatpak update check

--- src/XIVLauncher.Core/CoreEnvironmentSettings.cs
+++ src/XIVLauncher.Core/CoreEnvironmentSettings.cs
@@ -7,7 +7,6 @@ public static class CoreEnvironmentSettings
     public static bool? IsDeck => CheckEnvBoolOrNull("XL_DECK");
     public static bool? IsDeckGameMode => CheckEnvBoolOrNull("XL_GAMEMODE");
     public static bool? IsDeckFirstRun => CheckEnvBoolOrNull("XL_FIRSTRUN");
-    public static bool IsUpgrade => CheckEnvBool("XL_SHOW_UPGRADE");
     public static bool ClearSettings => CheckEnvBool("XL_CLEAR_SETTINGS");
     public static bool ClearPrefix => CheckEnvBool("XL_CLEAR_PREFIX");
     public static bool ClearPlugins => CheckEnvBool("XL_CLEAR_PLUGINS");
@@ -16,19 +15,19 @@ public static class CoreEnvironmentSettings
     public static bool ClearAll => CheckEnvBool("XL_CLEAR_ALL");
     public static bool? UseSteam => CheckEnvBoolOrNull("XL_USE_STEAM"); // Fix for Steam Deck users who lock themselves out
     public static bool IsSteamCompatTool => CheckEnvBool("XL_SCT");
-    public static uint SteamAppId => GetAppId(System.Environment.GetEnvironmentVariable("SteamAppId"));
-    public static uint AltAppID => GetAppId(System.Environment.GetEnvironmentVariable("XL_APPID"));
+    public static uint SteamAppId => GetAppId(Environment.GetEnvironmentVariable("SteamAppId"));
+    public static uint AltAppID => GetAppId(Environment.GetEnvironmentVariable("XL_APPID"));
 
     private static bool CheckEnvBool(string key)
     {
-        string val = (System.Environment.GetEnvironmentVariable(key) ?? string.Empty).ToLower();
+        string val = (Environment.GetEnvironmentVariable(key) ?? string.Empty).ToLower();
         if (val == "1" || val == "true" || val == "yes" || val == "y" || val == "on") return true;
         return false;
     }
 
     private static bool? CheckEnvBoolOrNull(string key)
     {
-        string val = (System.Environment.GetEnvironmentVariable(key) ?? string.Empty).ToLower();
+        string val = (Environment.GetEnvironmentVariable(key) ?? string.Empty).ToLower();
         if (val == "1" || val == "true" || val == "yes" || val == "y" || val == "on") return true;
         if (val == "0" || val == "false" || val == "no" || val == "n" || val == "off") return false;
         return null;
@@ -44,14 +43,14 @@ public static class CoreEnvironmentSettings
     public static uint GetAppId(string? appid)
     {
         uint.TryParse(appid, out var result);
-        
+
         // Will return 0 if appid is invalid (or zero).
         return result;
     }
 
     public static string GetCType()
     {
-        if (System.OperatingSystem.IsWindows())
+        if (OperatingSystem.IsWindows())
             return "";
         var psi = new ProcessStartInfo("sh");
         psi.Arguments = "-c \"locale -a 2>/dev/null | grep -i utf\"";
