52aaabb589b1e7d8ff93ea3d35a1a0f99336ba0a
Zyian <adrian@zyian.dev>
2025-12-20 14:14:42 -0700

    polish branch selector UI

--- src/XIVLauncher.Core/Components/SettingsPage/DalamudBranchMetaSettingsEntry.cs
+++ src/XIVLauncher.Core/Components/SettingsPage/DalamudBranchMetaSettingsEntry.cs
@@ -5,9 +5,9 @@ namespace XIVLauncher.Core.Components.SettingsPage;
 
 public class DalamudBranchMetaSettingsEntry : SettingsEntry<string>
 {
-    public DalamudBranchMeta.Branch? SelectedBranch { get; set; }
+    private DalamudBranchMeta.Branch? SelectedBranch { get; set; }
 
-    public List<DalamudBranchMeta.Branch> Branches { get; set; } = [];
+    private List<DalamudBranchMeta.Branch> Branches { get; set; } = [];
 
     public DalamudBranchMetaSettingsEntry(string name, string description, Func<string> load, Action<string?> save)
         : base(name, description, load, save)
@@ -19,29 +19,29 @@ public class DalamudBranchMetaSettingsEntry : SettingsEntry<string>
 
     public override void Draw()
     {
-        ImGuiHelpers.TextWrapped(this.Name);
-        var currentBranch = this.Branches.Find(b => b.Track == this.InternalValue as string);
+        ImGuiHelpers.TextWrapped(Name);
+        var currentBranch = Branches.Find(b => b.Track == InternalValue as string);
         if (ImGui.BeginCombo($"###{Id.ToString()}", currentBranch?.DisplayName))
         {
-            foreach (var b in this.Branches)
+            foreach (var b in Branches)
             {
                 if (ImGui.Selectable(b.DisplayName, b.Track == currentBranch?.Track))
                 {
-                    this.SelectedBranch = b;
-                    this.InternalValue = b.Track;
+                    SelectedBranch = b;
+                    InternalValue = b.Track;
                 }
             }
             ImGui.EndCombo();
         }
         
         ImGui.PushStyleColor(ImGuiCol.Text, ImGuiColors.DalamudGrey);
-        ImGuiHelpers.TextWrapped(this.Description);
+        ImGuiHelpers.TextWrapped(Description);
         ImGui.PopStyleColor();
     }
 
     public override void Save()
     {
-        Program.Config.DalamudBetaKey = this.SelectedBranch.Key;
+        Program.Config.DalamudBetaKey = SelectedBranch?.Key;
         base.Save();
     }
 }
